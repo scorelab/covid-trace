f7372f98a0ec600fc90f47863f8ee5a7339c03a0
piyumaldk <piyumaldk@gmail.com>
2020-11-02 13:20:21 +0530

    History screen

--- mobile-app/lib/screens/history/history_screen.dart
+++ mobile-app/lib/screens/history/history_screen.dart
@@ -23,66 +23,84 @@ class _HistoryScreenState extends State<HistoryScreen> {
       ),
       bottomNavigationBar: AppBottomNavbar(),
       body: Column(
-            children: [
-              ButtonBar(
-                alignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  ButtonTheme(
-                    height: 30,
-                    minWidth: screenWidth * 0.38,
-                    child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0)),
-                      color: Theme.of(context).primaryColor,
-                      textColor: Colors.white,
-                      child: Text(
-                        'ALL RECORDS',
-                        textScaleFactor: 1,
-                      ),
-                      onPressed: () {
-                        //Call a function to view pass
-                      },
-                    ),
+        children: [
+          ButtonBar(
+            alignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              ButtonTheme(
+                height: 30,
+                minWidth: screenWidth * 0.47,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Colors.white,
+                  child: Text(
+                    'ALL RECORDS',
+                    textScaleFactor: 1,
                   ),
-                  ButtonTheme(
-                    height: 30,
-                    minWidth: screenWidth * 0.38,
-                    child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0)),
-                      color: Colors.grey,
-                      textColor: Colors.white,
-                      child: Text(
-                        'POSSIBLE EXPOSURES',
-                        textScaleFactor: 1,
-                      ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/checkout');
-                      },
-                    ),
-                  ),
-                ],
+                  onPressed: () {
+                    //Call a function to view pass
+                  },
+                ),
               ),
-              Divider(),
-              Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: 5,
-                  itemBuilder: (BuildContext context, int index){
-                    return Column(
-                      children: [
-                        ListTile(
-                          title: Text('hi'),
-                        )
-                      ],
-                    );
+              ButtonTheme(
+                height: 30,
+                minWidth: screenWidth * 0.47,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  color: Colors.grey,
+                  textColor: Colors.white,
+                  child: Text(
+                    'POSSIBLE EXPOSURES',
+                    textScaleFactor: 1,
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/checkout');
                   },
                 ),
               ),
             ],
-
           ),
-
+          Divider(),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  color: Theme.of(context).accentColor,
+                  elevation: 2.0,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.transparent,
+                      child: Icon(
+                        Icons.location_on_rounded,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    title:
+                        Text(
+                            "University of Colombo", //data.name[index]
+                            style: TextStyle(color: Colors.black, fontSize: 16)
+                        ),
+                    subtitle:
+                        Text(
+                            "2020-08-02\n" //data.date[index]
+                                "From "
+                                "9.30 AM " //data.time[index]
+                                "to "
+                                "10.00 AM", //data.time[index]
+                            style: TextStyle(color: Colors.black54)
+                        ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
