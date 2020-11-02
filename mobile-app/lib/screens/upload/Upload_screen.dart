2710534e6db0ea3b6a41b34e10dcb4877b2b3827
Kalana Mihiranga <kalanam217@gmail.com>
2020-11-02 07:46:59 +0530

    Add Changes

--- mobile-app/lib/screens/upload/Upload_screen.dart
+++ mobile-app/lib/screens/upload/Upload_screen.dart
@@ -32,7 +32,7 @@ class _UploadScreenState extends State<UploadScreen> {
       ),
       bottomNavigationBar: AppBottomNavbar(),
       body: Padding(
-        padding: EdgeInsets.only(top: screenHeight * 0.05, left: 10, right: 10),
+        padding: EdgeInsets.only(top: screenHeight * 0.06, left: 10, right: 10),
         child: Center(
           child: ListView(
             children: <Widget>[
@@ -42,7 +42,7 @@ class _UploadScreenState extends State<UploadScreen> {
                   children: [
                     image1,
                     Container(
-                      height: 120,
+                      height: 100,
                       width: screenWidth * 0.8,
                       decoration: BoxDecoration(
                         boxShadow: [
@@ -55,18 +55,18 @@ class _UploadScreenState extends State<UploadScreen> {
                         color: Color(0xffd9d9d9),
                       ),
                       child: Padding(
-                        padding: const EdgeInsets.all(8.0),
+                        padding: const EdgeInsets.all(5.0),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.start,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             SizedBox(
-                              height: 5,
+                              height: 2,
                             ),
                             Text(
                               "An upload code is given to patients with Covid-19.",
                               style: TextStyle(
-                                  fontWeight: FontWeight.normal, fontSize: 16),
+                                  fontWeight: FontWeight.normal, fontSize: 13),
                             ),
                             SizedBox(
                               height: 10,
@@ -85,20 +85,20 @@ class _UploadScreenState extends State<UploadScreen> {
                     ),
                     _infected
                         ? Text(
-                            "We have identified you as a patient \n Use this code:- $_code ",
+                            "You are infected with Covid-19 \n Type this code to upload your data for contact tracing \n $_code",
                             textAlign: TextAlign.center,
                             style: TextStyle(
-                                fontWeight: FontWeight.w500,
+                                fontWeight: FontWeight.bold,
                                 color: Colors.red,
-                                fontSize: 20),
+                                fontSize: 21),
                           )
                         : Text(
-                            "You are not infected",
+                            "You are not infected with Covid-19",
                             textAlign: TextAlign.center,
                             style: TextStyle(
-                                fontWeight: FontWeight.w500,
+                                fontWeight: FontWeight.bold,
                                 color: Color(0xff626262),
-                                fontSize: 25),
+                                fontSize: 22),
                           ),
                     SizedBox(
                       height: 10,
